bc3e7564a2f2cc4de87b271b1aa40b828c694917
Rugved7 <rugvedagasti7@gmail.com>
2024-10-19 19:32:58 +0530

    Database connected

--- server/libs/connectDB.js
+++ server/libs/connectDB.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        await mongoose.connect(process.env.DATABASE_URI)
+        console.log("Database connected successfully");
 
     } catch (error) {
-        console.log("Error Connecting to Database");
-
+        console.log("Error Connecting to Database", error);
+        process.exit(1)
     }
 }
 export { connectDB }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
+import { connectDB } from "./libs/connectDB.js"
 
 dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-
+    console.log(`Server is listening on port ${PORT}`)
+    connectDB()
 })
